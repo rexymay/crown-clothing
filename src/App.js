c3df6654bc82ee8e533b48f71fa6140fa44a522d
Rex Sacayan <rex.sacayan@g.msuiit.edu.ph>
2020-07-02 21:40:10 +0800

    redirect to homepage if already signed-in, added action types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -40,21 +40,28 @@ class App extends React.Component
   }
 
   render(){
+    const { currentUser } = this.props;
     return (
       <div>
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/sign-inup' component={SignInUpPage} />
+          <Route exact path='/sign-inup' 
+            render={ () => currentUser ? (<Redirect to='/' />): (<SignInUpPage />) } 
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
